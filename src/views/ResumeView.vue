f4035772ed4981df5569f7e591690ae6f31321df
Sihle Mbobo <khwezimbobo@gmail.com>
2024-01-09 22:18:10 +0200

    added skills

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -9,69 +9,45 @@
             <div class="skills">
                 <div class="row justify-content-center">
                     <div class="col-lg-3 col-md-6 mb-2">
-                        <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                        <div class="card" style="width: 18rem; display: inline-block;">
+                            <img src="https://i.postimg.cc/DzMNpds4/pngwing-com-1-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block;">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/LsYD5dYh/html5-logo-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/BZgTqZYM/bootstrap-logo-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/Tw2PQxV3/css3-logo-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                 </div>
                 <div class="row justify-content-center mt-4">
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/HnRsp7fr/Vue-js-Logo-2-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block;">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/nhRZZmBp/Js-logo-512-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
                         <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                            <img src="https://i.postimg.cc/YqDyCznn/nodejs-icon-modified.png" height="240px" class="card-img-top" alt="">
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 mb-2">
-                        <div class="card" style="width: 18rem; display: inline-block">
-                            <img src="" class="card-img-top" alt="">
-                            <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
+                        <div class="card" style="width: 18rem; display: inline-block;">
+                            <img src="https://i.postimg.cc/85s7NDjn/mysql-icon.png" height="240px" class="card-img-top bg-light" alt="">
                         </div>
                     </div>
                 </div>
